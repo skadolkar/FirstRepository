5a5d007d69121348b5ee6c0bd0e272164c784ac8
skadolkar <skadolkar@gmail.com>
2025-11-19 22:43:13 +0530

    Message for Practice

--- src/main/java/org/openapitools/Practice.java
+++ src/main/java/org/openapitools/Practice.java
@@ -4,6 +4,7 @@ public class Practice {
 
     public static void main(String[] args) {
         System.out.println("Hello, Shubham Bharama Kadolkar! Have a Great Year Ahead and Happy Coding");
+        system.out.println("This is a practice class.");
     }
 
 }
