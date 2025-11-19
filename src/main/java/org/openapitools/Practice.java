2f80bc730afeb61a9075047b83e0946988b89b05
skadolkar <skadolkar@gmail.com>
2025-11-19 22:26:09 +0530

    Added full Name

--- src/main/java/org/openapitools/Practice.java
+++ src/main/java/org/openapitools/Practice.java
@@ -3,7 +3,7 @@ package org.openapitools;
 public class Practice {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Shubham Kadolkar!");
+        System.out.println("Hello, Shubham Bharama Kadolkar!");
     }
 
 }
