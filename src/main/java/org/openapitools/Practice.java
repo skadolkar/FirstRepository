9413a469436535bbf815ee446efc9cad9a71b972
skadolkar <skadolkar@gmail.com>
2025-11-19 22:40:01 +0530

    Changes made into Practice File as Hello Shubham Bharama Kadolkar and a Sweet Message

--- src/main/java/org/openapitools/Practice.java
+++ src/main/java/org/openapitools/Practice.java
@@ -3,7 +3,7 @@ package org.openapitools;
 public class Practice {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Shubham Bharama Kadolkar!");
+        System.out.println("Hello, Shubham Bharama Kadolkar! Have a Great Year Ahead and Happy Coding");
     }
 
 }
